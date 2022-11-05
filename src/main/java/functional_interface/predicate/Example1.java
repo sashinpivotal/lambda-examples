99f3d8210b8494cb1686d240a8a95c2f318d25b5
Sang Shin <sangshinpassion@gmail.com>
2022-11-05 13:32:03 -0400

    change

--- src/main/java/functional_interface/predicate/Example1.java
+++ src/main/java/functional_interface/predicate/Example1.java
@@ -10,13 +10,13 @@ public class Example1 {
 	public static void main(String[] args) {
 
 		Predicate<Double> predicate1 = x -> x > 10;
-		System.out.println(predicate1.test(new Double(9)));
+		System.out.println(predicate1.test(Double.valueOf(9.0)));
 		
 		Predicate<String> predicate2 = x -> x.length() > 5;
 		System.out.println(predicate2.test("JPassion.com"));
 		
 		BiPredicate<Double, Double> predicate3 = (x, y) -> x + y > 10;
-		System.out.println(predicate3.test(new Double(3), new Double(2)));
+		System.out.println(predicate3.test(Double.valueOf(3), Double.valueOf(2)));
 		
 		BiPredicate<String, Integer> predicate4 = (x, y) -> x.length() > y;
 		System.out.println(predicate4.test("JPassion.com", 5));
@@ -26,14 +26,14 @@ public class Example1 {
 		//   false otherwise
 		Predicate<Date> myPredicate = date -> {
 			// Set current time
-			Calendar calendarCurrenttime = Calendar.getInstance();
-			calendarCurrenttime.setTime(date);
+			Calendar calendarCurrentTime = Calendar.getInstance();
+			calendarCurrentTime.setTime(date);
 			
 			// Set noon
 			Calendar calendarNoon = Calendar.getInstance();
-			calendarCurrenttime.set(Calendar.HOUR_OF_DAY, 12);
+			calendarCurrentTime.set(Calendar.HOUR_OF_DAY, 12);
 			
-			return calendarCurrenttime.before(calendarNoon);
+			return calendarCurrentTime.before(calendarNoon);
 		};
 		
 		System.out.println(myPredicate.test(new Date()));
